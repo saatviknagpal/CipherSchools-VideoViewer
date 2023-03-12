0b3fe4497494b477edc8d3fb46c5f9f914c6d6eb
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2023-03-12 16:13:54 +0530

    fix

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const User = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
